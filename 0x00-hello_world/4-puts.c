a112d0ff8d2322ea8b38ce105b6928841995f404
Jmucheke <jmucheke274@gmail.com>
2021-08-26 11:59:39 -0700

    task 0 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * String initialisation
+ * writing strin to standard output
+ */
+int main()
+{
+	char my_str[] = "Programming is like building a multilingual puzzle";
+	puts(my_str);
+
+	return (0);
+}
