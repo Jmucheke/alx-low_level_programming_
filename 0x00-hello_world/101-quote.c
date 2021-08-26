8d6c7c4e5dd8f88fb714f95f5785bc253bd79e71
Jmucheke <jmucheke274@gmail.com>
2021-08-26 13:48:25 -0700

    fixing task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * unistd.h library added for the write function
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+
 	return (1);
 }
 
